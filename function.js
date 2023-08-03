197a625955f75c011dbb4fb4a852dc2d11795115
Talha Khan <talhakhan2016thk@gmail.com>
2023-08-03 17:05:42 +0500

    3 aug

--- function.js
+++ function.js
@@ -136,3 +136,93 @@ function addTwoNumbers(x, y) {
 }
 
 */
+
+
+// --------------------------- 3 AUG 2023 ---------------------------
+
+/*
+function testAvailability() {
+    let y = "I'll return";
+    console.log("Available here:", y);
+    return y;
+   }
+   let z = testAvailability();
+   console.log("Outside the function:", z);
+   //console.log("Not available here:", y);
+*/
+
+/*
+let val = 10;
+function tester(val){
+ val += 10;
+ if(val < 100){
+ return tester(val);
+ }
+ return val;
+ 
+}
+tester(val);
+console.log(val);
+*/
+
+/*
+let testFunction = function(){
+    console.log("Hello");
+   }();
+*/
+
+
+/*
+(function () {
+    console.log("Welcome");
+   })();
+   (function () {
+    let firstName = "Laurence";
+   })();
+   let result = (function () {
+    let firstName = "Laurence";
+    return firstName;
+   })();
+   console.log(result);
+   (function (firstName) {
+    console.log("My Name is " + firstName);
+   })("Laurence");
+   
+*/
+
+/*
+let test2 = (num) => num + 5;
+console.log(test2(14));
+*/
+
+
+/*
+var addFive1 = function addFive1(num) {
+    return num + 2;
+    };
+    let addFive2 = (num) => num + 2;
+    console.log(addFive1(14));
+*/
+
+// ========================================== EX: 6.5 ===========================================
+
+let value = "1000";
+(function () {
+    let value = "2000"
+    console.log(value);
+   })();
+
+let result = (function () {
+    let value = "result values"
+    return value;
+   })();
+   console.log(value);
+   console.log(result);
+
+   (function (val) {
+    console.log(`My name is ${val}`);
+   })("talha");
+
+   
+
+
